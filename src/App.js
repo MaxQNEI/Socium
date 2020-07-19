7ff94456163afd1c83e356630f143d0c7793ddf3
MaxQNEI <maxqnei@gmail.com>
2020-07-19 14:51:11 +0300

    Add raycasting (+pointer)

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default class App {
 
     this.Canvas.classList.add('root-canvas');
 
-    window.addEventListener('resize', (evt) => {
+    window.addEventListener('resize', (event) => {
       this.Renderer.setSize(this.Width(), this.Height());
 
       this.Camera.aspect = this.Aspect();
@@ -48,23 +48,26 @@ export default class App {
   Scenario() {
     this.Scene.fog = new THREE.Fog(0x000000, 100, 500);
 
-    // ―――― Plane ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――― //
-    const PlaneMesh = new THREE.Mesh(
-      new THREE.PlaneGeometry(1000, 1000, 100, 100),
+    // ―――― BuildGrid ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――― //
+    const BuildGridMesh = new THREE.Mesh(
+      new THREE.PlaneBufferGeometry(100, 100, 10, 10),
       new THREE.MeshBasicMaterial({
         color: '#ffffff',
-        wireframe: true,
+        // wireframe: true,
+        transparent: true,
+        opacity: .2,
       }),
     );
-    PlaneMesh.rotation.x = -(Math.PI / 2);
+    BuildGridMesh.rotation.x = -(Math.PI / 2);
 
-    const PlaneWrap = new THREE.Object3D;
-    PlaneWrap.add(PlaneMesh);
+    const BuildGridWrap = new THREE.Object3D;
+    BuildGridWrap.add(BuildGridMesh);
 
-    this.Scene.add(PlaneWrap);
+    this.Scene.add(BuildGridWrap);
     // ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――― //
 
 
+    // ―――― Object to observation ――――――――――――――――――――――――――――――――――――――――――― //
     const CubeSize = 5;
     const CubeMesh = new THREE.Mesh(
       new THREE.BoxGeometry(CubeSize, CubeSize, CubeSize),
@@ -75,19 +78,66 @@ export default class App {
 
     this.Scene.add(CubeMesh);
 
-
     this.ZeroPosition = new THREE.Vector3(0, 0, 0);
 
-    (this.Scene.AnimationList = (this.Scene.AnimationList = []))
-      .push((timestamp) => {
-        this.Camera.position.y = (Math.sin(timestamp / 5e3) + 50);
-        this.Camera.position.x = (Math.cos(timestamp / 2e3) * 50);
-        this.Camera.position.z = (Math.sin(timestamp / 2e3) * 50);
-        // this.Camera.rotation.x = (timestamp / 3e3);
-        // this.Camera.rotation.y = (timestamp / 1e3);
+        this.Camera.position.y = 50;
+        this.Camera.position.x = 50;
+        this.Camera.position.z = 50;
         this.Camera.lookAt(this.ZeroPosition);
+
+    // Need create extented scene object (project)
+    // (this.Scene.AnimationList = (this.Scene.AnimationList || []))
+    //   .push((timestamp) => {
+    //     this.Camera.position.y = (Math.sin(timestamp / 5e3) + 50);
+    //     this.Camera.position.x = (Math.cos(timestamp / 2e3) * 50);
+    //     this.Camera.position.z = (Math.sin(timestamp / 2e3) * 50);
+    //     // this.Camera.rotation.x = (timestamp / 3e3);
+    //     // this.Camera.rotation.y = (timestamp / 1e3);
+    //     this.Camera.lookAt(this.ZeroPosition);
+    //   })
+    // ;
+    // ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――― //
+
+
+    // ―――― "Building" ―――――――――――――――――――――――――――――――――――――――――――――――――――――― //
+    const BuildPointer = new THREE.Mesh(
+      new THREE.CylinderBufferGeometry(0.1, 0.1, 20, 20),
+      new THREE.MeshBasicMaterial({ color: 'red' })
+    );
+
+    this.Scene.add(BuildPointer);
+
+    const raycaster = new THREE.Raycaster();
+    const mouse = new THREE.Vector2();
+    var intersects;
+
+    window.addEventListener('touchmove', (event) => {
+      const touch = event.touches[0];
+      mouse.x = (touch.clientX / window.innerWidth) * 2 - 1;
+      mouse.y = - (touch.clientY / window.innerHeight) * 2 + 1;
+    });
+    window.addEventListener('mousemove', (event) => {
+      mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+    });
+
+    // Need create extented scene object (project)
+    (this.Scene.AnimationList = (this.Scene.AnimationList || []))
+      .push((timestamp) => {
+        raycaster.setFromCamera(mouse, this.Camera);
+        intersects = raycaster.intersectObject(BuildGridWrap, true);
+
+        (intersects && intersects[0]) && (() => {
+          var { x, y, z } = intersects[0].point;
+
+          x = Math.round(x);
+          z = Math.round(z);
+
+          BuildPointer.position.set(x, y, z);
+        })();
       })
     ;
+    // ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――― //
   }
 
   // Base.js
@@ -122,7 +172,7 @@ export default class App {
 
 
   // Additionals
-  LockEvent(evt) {
-    evt.preventDefault();
+  LockEvent(event) {
+    event.preventDefault();
   }
 }
